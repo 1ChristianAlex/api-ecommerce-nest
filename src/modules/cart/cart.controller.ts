32ffb95244841dc0ceb70bf62c132b1b0117a034
Christian Alexsander Santos <christianalexbh@hotmail.com>
2022-07-19 01:35:21 -0300

    docker context

--- src/modules/cart/cart.controller.ts
+++ src/modules/cart/cart.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { UserRequest } from '../auth/auth.requestDecorator';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { UserModel } from '../user/user.model';
